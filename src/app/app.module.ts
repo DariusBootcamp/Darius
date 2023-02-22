1c10abddd921fafa8f4d2bc7e816d99d15351dbd
Darius Dang <dariusbc2023@gmail.com>
2023-02-22 16:50:15 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UsersComponent,
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,44 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+  title = 'admindashboard';
+
+
+
+  // responsivemenu
+  responsiveMenu:any;
+  // responsivemaincontent
+  responsiveContent:any;
+  defaultStatus=true;
+  openNav(status:any)
+  {
+    if(status===this.defaultStatus)
+    {
+      this.responsiveMenu = {
+        'display':'block',
+        'background-color' : '#11101D',
+        'color' :'#fff'
+      }
+      this.responsiveContent={
+        'margin-left':'150px'
+      }
+      this.defaultStatus = false;
+    }else
+    {
+      this.responsiveMenu = {
+        'display':null
+      }
+      this.responsiveContent={
+        'margin-left':null
+      }
+      this.defaultStatus=true;
+    }
+  }
+
+}
